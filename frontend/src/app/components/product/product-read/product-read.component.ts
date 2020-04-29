0d244f65ef83f82416635fdeaa17255c478ba193
Daniele Leite <daniele.santos@vixsystem.com.br>
2020-04-29 15:57:55 -0300

    Desafio Método Excluir Produto

--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { ProductService } from '../product.service';
 import { Product } from '../product.model';
@@ -14,7 +15,8 @@ export class ProductReadComponent implements OnInit {
   displayedColumns: string[] = ['id', 'name', 'price', 'action'];
 
   constructor(
-    private productService: ProductService
+    private productService: ProductService,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -24,6 +26,13 @@ export class ProductReadComponent implements OnInit {
         this.products = products;
         console.log(products);
       });
+  }
+
+  delete(id: string) {
+    this.productService.delete(id).subscribe(() => {
+      this.productService.showMessage('Produto deletado com sucesso!');
+      this.router.navigate(['/products']);
+    })
   }
 
 }
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -41,4 +41,9 @@ export class ProductService {
     const url = `${this.baseUrl}/${product.id}`;
     return this.http.put<Product>(url, product);
   }
+
+  delete(id: string): Observable<Boolean> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Boolean>(url);
+  }
 }
